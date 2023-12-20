91876abce4071eebbbe418c8d8bc4a89138f2521
kataflam <154386598+kataflam@users.noreply.github.com>
2023-12-20 02:15:40 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,8 @@
-
 import streamlit as st
 
 # Define the list of items
 items = {
-    1: "In the museum where Ross works",
+     1: "In the museum where Ross works",
     2: "Beach house",
     3: "Weekend at Bernies",
     4: "مونا - Mona",
@@ -13,19 +12,19 @@ items = {
     8: "Bonnie and Julie",
     9: "Brad Pitt",
     10: "Van Damme",
-
 }
 
 def main():
     st.title("Number Text Answer")
 
     user_input = st.number_input("Enter a number from 1 to 300:", min_value=1, max_value=300, step=1)
-    answer = items.get(int(user_input), 'No information available for this number.')
+    submitted = st.button("Submit Answer")
 
-    st.write(f'Text answer: {answer}')
+    if submitted:
+        answer = items.get(int(user_input), 'No information available for this number.')
+        st.write(f'Text answer: {answer}')
 
 if __name__ == '__main__':
     main()
-
 
 
