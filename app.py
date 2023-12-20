6c9caa54d897ef170b4dfda30c8d67510bdddd7c
kataflam <154386598+kataflam@users.noreply.github.com>
2023-12-20 02:34:01 +0200

    Update app.py

--- app.py
+++ app.py
@@ -2,16 +2,10 @@ import streamlit as st
 
 # Define the list of items
 items = {
-    1: "In the museum where Ross works",
-    2: "Beach house",
-    3: "Weekend at Bernies",
-    4: "مونا - Mona",
-    5: "Bicycle",
-    6: "Frank Jr. Jr., Leslie, Chandler",
-    7: "Dentist",
-    8: "Bonnie and Julie",
-    9: "Brad Pitt",
-    10: "Van Damme",
+ 1: "In the museum where Ross works",
+2: "Beach house",
+3: "Weekend at Bernies",
+4: "مونا - Mona"
 }
 
 def main():
@@ -20,7 +14,7 @@ def main():
         """
         <style>
             body {
-                background-image: url('web1.png');  /* Change the path accordingly */
+                background-image: url('web1.png');  /* Updated to use web1.png in the same directory */
                 background-size: cover;
             }
         </style>
@@ -31,9 +25,11 @@ def main():
     st.title("Number Text Answer")
 
     user_input = st.number_input("Enter a number from 1 to 300:", min_value=1, max_value=300, step=1)
+    show_answer = st.button("Show Answer")
 
-    answer = items.get(int(user_input), 'No information available for this number.')
-    st.write(f'Text answer: {answer}')
+    if show_answer:
+        answer = items.get(int(user_input), 'No information available for this number.')
+        st.write(f'Text answer: {answer}')
 
 if __name__ == '__main__':
     main()
